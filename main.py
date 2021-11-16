5ca8f2f019d3e44ce37995eca57e3f18e76aaff2
sgiardl <sgiardleroux@gmail.com>
2021-11-15 19:13:36 -0500

    Implemented function to split dataframes by n samples

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ Olivier Lefebvre
 Simon Giard-Leroux
 """
 
-from src.dataframes import load_data
+from src.dataframes import load_data, split_data
 
 if __name__ == '__main__':
     data = load_data('data/res_2000.csv')
 
-    print(data)
+    data_split = split_data(data, 21)
+
+    print(data_split)
--- src/dataframes.py
+++ src/dataframes.py
@@ -7,6 +7,8 @@ Simon Giard-Leroux
 """
 
 import pandas as pd
+import numpy as np
+from math import ceil
 
 
 def load_data(filepath: str) -> pd.DataFrame:
@@ -15,3 +17,13 @@ def load_data(filepath: str) -> pd.DataFrame:
 
     except FileNotFoundError:
         raise FileNotFoundError(f"Please download the dataset and save it as '{filepath}' to use this script")
+
+
+def split_data(df: pd.DataFrame,
+               window_size: int) -> dict:
+    data_list = []
+
+    for i in range(ceil(len(df) / window_size)):
+        data_list.append(df[i * window_size:(i + 1) * window_size])
+
+    return data_list
